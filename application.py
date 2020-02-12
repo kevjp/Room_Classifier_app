c0b5e518426ac7b08dd75459c17206102d8076f4
kevjp <kevin@ryancodingdesign.com>
2020-02-12 15:02:50 +0000

    removed threaded=False setting

--- application.py
+++ application.py
@@ -125,5 +125,5 @@ image_url = TextField('Name:', validators=[validators.required()])
 
 
 if __name__ == '__main__':
-    application.run(use_reloader=False, threaded=False)
+    application.run(use_reloader=False)
     # application.run(host='0.0.0.0', use_reloader=False)
